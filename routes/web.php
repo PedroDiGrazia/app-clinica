1b98753509d00cd5784f28840f42d08c022a7178
PedroDiGrazia <pedrodigrazia@gmail.com>
2024-05-08 21:37:29 -0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testroute', function () {
+    Mail::to(users: 'mailtrap.club@gmail.com')->send(new \App\Mail\MyTestEmail());
+});
